533d42335c79778ca560291c19251cacb045f1fd
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2013-03-05 10:56:43 -0800

    Finish reorganizing; finished ps4a

--- problem-set/ps4/ps4a.py
+++ problem-set/ps4/ps4a.py
@@ -298,12 +298,28 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print "playGame not yet implemented." # <-- Remove this line when you code the function
-   
+    choice = ''
+    hand = {}
 
+    while choice != 'e':
+        choice = raw_input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ").lower()
 
-
+        if choice == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+        elif choice == 'r':
+            if hand.keys() == []:
+                print "You have not played a hand yet. Please play a new hand first!"
+                print
+                next
+            else:
+                playHand(hand, wordList, HAND_SIZE)
+        elif choice == 'e':
+            break
+        else:
+            print "Invalid command."
+            next
+            
 #
 # Build data structures used for entire session and play game
 #
