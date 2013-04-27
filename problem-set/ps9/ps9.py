1e45c291f49f722fa204c2e2c1358ba435c46c78
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2013-04-26 18:09:30 -0700

    Start work on ps9

--- problem-set/ps9/ps9.py
+++ problem-set/ps9/ps9.py
@@ -21,10 +21,76 @@ def simulationDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
+    # Initialize parameters from ps8
+    numViruses = 100
+    maxPop = 1000
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol': False}
+    mutProb = 0.005
+
+
+    viruses = []
+    for nv in range(numViruses):
+        viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+
+    preTreatmentTimeSteps = [300, 150, 75, 0]
+    postTreatmentTimeSteps = 150
+    outcomeTypes = ['Total', 'Resistant']
+    outcomes = {}
+
+    # Initialize outcomes to store results
+    # outcomes is a dict to store the populations of average and resistant
+    # viruses. Each element stores a dict with four arrays of the total viruses
+    # found at each time step, keyed by the number of steps prior to treatment.
+    # Thus each array will have len() of preTreatmentSteps + 150.
+    for outcomeType in outcomeTypes:
+        outcomes[outcomeType] = {}
+        for pre in preTreatmentTimeSteps:
+            outcomes[outcomeType][pre] = []
+            for preTimeStep in range(pre + postTreatmentTimeSteps):
+                outcomes[outcomeType][pre].append(0)
+
+    for pre in preTreatmentTimeSteps:
+
+        for numTrial in range(numTrials):
+            #print "PreTreatTimeSteps {0} - Trial # {1}".format(pre, numTrial)
+            patient = TreatedPatient(viruses[:], maxPop)
+
+            for preTimeStep in range(pre):
+                patient.update()
+                outcomes['Total'][pre][preTimeStep] += patient.getTotalPop()
+                outcomes['Resistant'][pre][preTimeStep] += patient.getResistPop(['guttagonol'])
+
+            patient.addPrescription('guttagonol')
+
+            for postTimeStep in range(postTimeSteps):
+                patient.update()
+                outcomes['Total'][pre][postTimeStep] += patient.getTotalPop()
+                outcomes['Resistant'][pre][postTimeStep] += patient.getResistPop(['guttagonol'])
+
+    for outcomeType in outcomeTypes:
+        for pre in preTreatmentTimeSteps:
+            for preTimeStep in range(pre + postTreatmentTimeSteps):
+                outcomes[outcomeType][pre] /= numTrials
+
+    #print "Average population (length = {1}) = {0}".format(averagePopulation, len(averagePopulation))
+    #print "Average resistant population (length = {1}) = {0}".format(resistantPopulation, len(resistantPopulation))
+
+    pylab.figure(1)
+    for outcomeType in outcomeTypes:
+        for pre in preTreatmentTimeSteps:
+            pylab.plot(outcomes[outcomeType][pre], label="{0} population - {1} steps before treatment".format(
+                outcomeType, pre)
+    pylab.plot(resistantPopulation, 'g-', label='Average resistant population')
+    pylab.xlabel("Time Steps")
+    pylab.ylabel("Virus Population")
+    pylab.title("ResistantVirus simulation")
+    pylab.legend(loc='best')
+    pylab.show()
     
-    # TODO
 
-
+simulationDelayedTreatment(1)
 
 
 
